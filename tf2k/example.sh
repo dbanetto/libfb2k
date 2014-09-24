fd44b7680788a10e3cce9022bd45a0264aede07b
David Barnett <david@barnett.net.nz>
2014-09-24 19:11:02 +1200

    Fixed tf2k example

--- tf2k/example.sh
+++ tf2k/example.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-LD_LIBRARY_PATH=./Debug/
-
-./Debug/tf2k -e "%artist%/\$if(%album%,%album%/,)%title%" -f test.flac
+../build/tf2k/tf2k -e "%artist%/\$if(%album%,%album%/,)%title%" -f test.flac
